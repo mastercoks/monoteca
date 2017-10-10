4329b83801636f920bfb22c2068f4698a8b2e931
mastercoks <matheusbdo1@gmail.com>
2017-10-09 23:32:57 -0300

    CRUD usuario + sistema de autenticação

---
+++ config.js
@@ -0,0 +1,12 @@
+/**
+ * Arquivo: config.js
+ * Data: 09/10/2017
+ * Observação: Arquivo será responsável por armazenar diferentes variáveis e principalmente a configuração da nossa aplicação.
+ * Author: Matheus Coqueiro Andrade
+ */
+
+module.exports = {
+  'configName': 'monoteca',
+  'database_remoto' : 'mongodb://mastercoks:=e2E&raspA7e@ds113835.mlab.com:13835/monoteca' // via mLab
+  'database_local': 'mongodb://localhost/monoteca' // localmente
+}
